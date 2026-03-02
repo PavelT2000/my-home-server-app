88a0e85dda4fba8a6a1cc40d5d41b4deb7326e3a
PavelT2000 <pavelzabelich228@gmail.com>
2026-03-02 13:54:35 +0300

    first version

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "message": "Привет с Orange Pi через CI/CD!"})
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/author")
+async def author(request: Request):
+    return templates.TemplateResponse("author.html", {"request": request})
+
+@app.get("/work")
+async def work(request: Request):
+    return templates.TemplateResponse("work.html", {"request": request})
---
+++ static/sync.bat
@@ -0,0 +1,2 @@
+scp -r .\* orangepi@100.90.253.85:~/My/webSite/static/
+pause
